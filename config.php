98a735d0d2f5d2c0b54a1a3132d918f8da949b6e
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2022-06-27 15:02:17 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 $config = array();
-$config['name'] = "Plesk Login";
+$config['name'] = "Server login with token";
 $config['author'] = "Bozhidar Slaveykov";
 $config['ui'] = false; //if set to true, module will be visible in the toolbar
 $config['ui_admin'] = false; //if set to true, module will be visible in the admin panel
 $config['categories'] = "content";
 $config['position'] = 99;
-$config['version'] = 0.3;
+$config['version'] = 0.4;
 
 $config['tables'] = array(
     'users_temp_login_tokens' => [
