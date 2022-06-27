f1f57f681c49396056b8a3aff035360c40765186
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2022-06-27 14:31:06 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -33,30 +33,44 @@
     }
 
 });
+
+\Artisan::command('microweber:generate-admin-login-token-url', function() {
+   
+    $token = ___generateAdminLoginToken();
+    
+    return site_url(). '/api/user_login?secret_key=' . $token;
+});
+
 \Artisan::command('microweber:generate-admin-login-token', function() {
 
-    // Generate token
-    $generateToken = str_random(123);
-
-    // Find first admin
-    $firstAdmin = get_users('is_admin=1&single=1&is_active=1');
-    if (!$firstAdmin) {
-        return false;
-    }
-
-    $saveToken = array();
-    $saveToken['token'] = $generateToken;
-    $saveToken['user_id'] = $firstAdmin['id'];
-    $saveToken['created_at'] = date('Y-m-d H:i:s');
-    $saveToken['server_ip'] = user_ip();
-
-    // Save temp token
-    $save = db_save('users_temp_login_tokens', $saveToken);
-    if ($save) {
-        echo $generateToken;
-    }
-
+    return ___generateAdminLoginToken();
+   
 });
+
+function ___generateAdminLoginToken() {
+if ( app()->runningInConsole() ){
+         // Generate token
+        $generateToken = str_random(123);
+
+        // Find first admin
+        $firstAdmin = get_users('is_admin=1&single=1&is_active=1');
+        if (!$firstAdmin) {
+            return false;
+        }
+
+        $saveToken = array();
+        $saveToken['token'] = $generateToken;
+        $saveToken['user_id'] = $firstAdmin['id'];
+        $saveToken['created_at'] = date('Y-m-d H:i:s');
+        $saveToken['server_ip'] = user_ip();
+
+        // Save temp token
+        $save = db_save('users_temp_login_tokens', $saveToken);
+        if ($save) {
+            echo $generateToken;
+        }
+    }
+}
 
 
 event_bind('mw.user.before_login', function ($params = false) {
